89849f2955e0313d302477f264636aa3c815e4e5
sujade <dnmzsu@gmail.com>
2024-03-29 14:44:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// React
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//Font
+import '@fontsource/atkinson-hyperlegible';
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Style
+import "./App.css"
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+
+  </React.StrictMode>,
+)
